02c46d64e9eb85ce1c06cafda25dc29c9974d47d
Mahmoud <ma7moudaboelazm@gmail.com>
2021-03-29 10:07:10 +0200

    set issue in the color mode

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,24 +1,14 @@
-import { ColorModeProvider } from "@chakra-ui/color-mode";
+import { ColorModeProvider, ColorModeScript } from "@chakra-ui/color-mode";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import theme from "../theme";
 import isServer from "../utils/isServer";
 
 function MyApp({ Component, pageProps }: any) {
-  let mode: any;
-  if (!isServer()) {
-    mode = localStorage.getItem("chakra-ui-color-mode");
-  }
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          initialColorMode: mode || "dark",
-          useSystemColorMode: false,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
